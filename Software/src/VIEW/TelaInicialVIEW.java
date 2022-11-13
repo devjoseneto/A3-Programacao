542d8c6d0da3f7356cd543f85326bd8a282332d2
José Neto <netoiga26@gmail.com>
2022-11-13 16:15:23 -0300

    LoginVIEW

--- Software/src/VIEW/TelaInicialVIEW.java
+++ Software/src/VIEW/TelaInicialVIEW.java
@@ -8,12 +8,12 @@ package VIEW;
  *
  * @author netoi
  */
-public class Home extends javax.swing.JFrame {
+public class TelaInicialVIEW extends javax.swing.JFrame {
 
     /**
      * Creates new form Home
      */
-    public Home() {
+    public TelaInicialVIEW() {
         initComponents();
     }
 
@@ -184,20 +184,21 @@ public class Home extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaInicialVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaInicialVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaInicialVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaInicialVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Home().setVisible(true);
+                new TelaInicialVIEW().setVisible(true);
             }
         });
     }
--- Software/src/classes/Principal.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class Principal {
-    public static void main(String[] args) {
-        
-    }
-    
-}
